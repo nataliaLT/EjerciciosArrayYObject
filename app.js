3e175a187cce7a39431f0791a6fd4921a5c1e328
natalia <natalia.lucerot@gmail.com>
2018-08-02 12:14:57 -0500

    agregando ejercicios de array

---
+++ app.js
@@ -0,0 +1,31 @@
+//Ejercicios de array
+//Ejercicio computeAverageLengthOfWords
+function computeAverageLengthOfWords(word1, word2) {
+    let resultado = (word1.length + word2.length)/2;
+    return resultado
+  }
+
+//Ejercicio getNthElement
+function getNthElement(array, n) {
+    let output = ([array], n);
+    for(let i >0; array.legnth; ++i);
+ }
+ console.log(output)
+
+ //Ejercicio convertDoubleSpaceToSingle
+ function convertDoubleSpaceToSingle(str){ 
+    let expresionRegular = /\s* \s*/;
+    return str.split(expresionRegular).join(' ');
+  }  
+
+//Ejercicio areValidCredentials
+function areValidCredentials(name, password) {
+    if(name.length > 3 && password.length >8){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+//Ejercicios de Objects
+  
